bbd96ec81765672d7297897fba6ad08f2daeb4c8
ArcObliviate <120854853+ArcObliviate@users.noreply.github.com>
2024-02-05 01:29:08 +0000

    Delete nvim directory

--- nvim/lua/arc/mason.lua
+++
@@ -1,5 +0,0 @@
-local mason = require("mason")
-
-mason.setup {
-
-}
--- nvim/lua/arc/oil.lua
+++
@@ -1,7 +0,0 @@
--- function getCurrentDir() 
---     local buf = vim.fn.expand('%:p')
---     local dir = vim.fn.fnamemodify(buf, ":h")
---     vim.cmd("Ex "..dir)
--- end
---
--- vim.keymap.set("n", "-", getCurrentDir)
